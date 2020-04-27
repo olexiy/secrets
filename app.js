2eaf253f97d4a9f70f7925c5bda3b07386838d8b
Olexiy Sokurenko <ol.sakura@gmail.com>
2020-04-27 14:12:39 +0200

    Hashing version of user authentication.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -30,8 +30,6 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret, encryptedFields: ['password'] });
 
 const User = mongoose.model('User', userSchema);
 
@@ -49,7 +47,7 @@ app.post('/login', (req, res) => {
     if (err) {
       console.log(err);
     } else if (foundUser) {
-      if (foundUser.password === password) {
+      if (foundUser.password === md5(password)) {
         res.render('secrets');
       }
     }
@@ -63,7 +61,7 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
   newUser.save((err) => {
     if (err) {
